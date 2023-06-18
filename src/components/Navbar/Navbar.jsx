1e95a2a45c9bb7d53c2b2769617e8cf58a56060a
Arjun Khanna <arjunkhanna0108@gmail.com>
2023-06-19 00:36:07 +0530

    feat: added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { MenuBar } from "./MenuBar.jsx";
+import { AvatarComp } from "./Avatar.jsx";
+import Image from "next/Image";
+import { FaBell } from "react-icons/fa";
+
+export const Navbar = () => {
+	return (
+		<div className="flex items-center justify-center bg-white p-[3px] shadow-[0_2px_10px] shadow-blackA7">
+			<div className="flex w-[90vw] h-[80px] gap-4 justify-between items-center ">
+				<div className="w-[150px] ">
+					<Image
+						className="h-[50px]"
+						src="/logo.svg"
+						width={10000}
+						height={10000}
+						alt="logo"
+					/>
+				</div>
+				<div className="justify-self-end w-[100vw]">
+					<MenuBar />
+				</div>
+				<div className="flex items-center gap-[10px]">
+					<FaBell size="1.5em" />
+					<AvatarComp />
+				</div>
+			</div>
+		</div>
+	);
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,105 @@
+// const plugin = require("tailwindcss/plugin");
+const { blackA, mauve, violet, indigo, purple } = require("@radix-ui/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/app/page.js",
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				...blackA,
+				...mauve,
+				...violet,
+				...purple,
+				...indigo,
+			},
+			keyframes: {
+				enterFromRight: {
+					from: { opacity: 0, transform: "translateX(200px)" },
+					to: { opacity: 1, transform: "translateX(0)" },
+				},
+				enterFromLeft: {
+					from: { opacity: 0, transform: "translateX(-200px)" },
+					to: { opacity: 1, transform: "translateX(0)" },
+				},
+				exitToRight: {
+					from: { opacity: 1, transform: "translateX(0)" },
+					to: { opacity: 0, transform: "translateX(200px)" },
+				},
+				exitToLeft: {
+					from: { opacity: 1, transform: "translateX(0)" },
+					to: { opacity: 0, transform: "translateX(-200px)" },
+				},
+				scaleIn: {
+					from: {
+						opacity: 0,
+						transform: "rotateX(-10deg) scale(0.9)",
+					},
+					to: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+				},
+				scaleOut: {
+					from: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+					to: {
+						opacity: 0,
+						transform: "rotateX(-10deg) scale(0.95)",
+					},
+				},
+				fadeIn: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+				fadeOut: {
+					from: { opacity: 1 },
+					to: { opacity: 0 },
+				},
+				slideDownAndFade: {
+					from: { opacity: 0, transform: "translateY(-2px)" },
+					to: { opacity: 1, transform: "translateY(0)" },
+				},
+				slideLeftAndFade: {
+					from: { opacity: 0, transform: "translateX(2px)" },
+					to: { opacity: 1, transform: "translateX(0)" },
+				},
+				slideUpAndFade: {
+					from: { opacity: 0, transform: "translateY(2px)" },
+					to: { opacity: 1, transform: "translateY(0)" },
+				},
+				slideRightAndFade: {
+					from: { opacity: 0, transform: "translateX(-2px)" },
+					to: { opacity: 1, transform: "translateX(0)" },
+				},
+			},
+		},
+		animation: {
+			scaleIn: "scaleIn 200ms ease",
+			scaleOut: "scaleOut 200ms ease",
+			fadeIn: "fadeIn 200ms ease",
+			fadeOut: "fadeOut 200ms ease",
+			enterFromLeft: "enterFromLeft 250ms ease",
+			enterFromRight: "enterFromRight 250ms ease",
+			exitToLeft: "exitToLeft 250ms ease",
+			exitToRight: "exitToRight 250ms ease",
+			slideDownAndFade:
+				"slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+			slideLeftAndFade:
+				"slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+			slideUpAndFade:
+				"slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+			slideRightAndFade:
+				"slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+		},
+	},
+	plugins: [
+		// plugin(({ matchUtilities }) => {
+		// 	matchUtilities({
+		// 		perspective: (value) => ({
+		// 			perspective: value,
+		// 		}),
+		// 	});
+		// }),
+	],
+};
